b21e0becee0f9b9705eb57133b948206399d6970
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-05-22 14:04:44 +0900

    update(PUT) を実装

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -55,5 +55,22 @@ namespace Commander.Controllers
             // return Ok(commandReadDto);
             return CreatedAtRoute(nameof(GetCommandById), new {Id = commandReadDto.Id}, commandReadDto);
         }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdateDto)
+        {
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if (commandModelFromRepo == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(commandUpdateDto, commandModelFromRepo);
+
+            _repository.UpdateCommand(commandModelFromRepo);
+
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
