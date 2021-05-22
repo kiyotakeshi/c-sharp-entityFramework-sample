41d74c9d996c9bf89e68a7ec0f0472a1b260b3a0
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-05-22 10:37:11 +0900

    POST による新規作成を実装

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -30,7 +30,7 @@ namespace Commander.Controllers
         }
 
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name="GetCommandById")]
         public ActionResult<CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
@@ -40,6 +40,20 @@ namespace Commander.Controllers
                 return Ok(_mapper.Map<CommandReadDto>(commandItem));
             }
             return NotFound();
+        }
+        
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto)
+        {
+            var commandModel = _mapper.Map<Command>(commandCreateDto);
+            _repository.CreateCommand(commandModel);
+            // これがないと永続化されない
+            _repository.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+
+            // return Ok(commandReadDto);
+            return CreatedAtRoute(nameof(GetCommandById), new {Id = commandReadDto.Id}, commandReadDto);
         }
     }
 }
